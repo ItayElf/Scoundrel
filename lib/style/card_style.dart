2f3136c2d4a941b54d2c1638e9b672c73ee09b77
איתי בן חיים <thh123321@gmail.com>
2025-02-22 16:26:18 +0200

    Feat: added slight tint

--- lib/style/card_style.dart
+++ lib/style/card_style.dart
@@ -1,6 +1,8 @@
+import 'package:flutter/material.dart';
 import 'package:playing_cards/playing_cards.dart';
 
 final defaultCardStyle = PlayingCardViewStyle(
+  surfaceTintColor: Colors.yellowAccent,
   suitStyles: {
     Suit.spades: SuitStyle(),
     Suit.clubs: SuitStyle(),
