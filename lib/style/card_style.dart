40c3e1186921f75c3c5e05d479bae9ab8a2b61af
איתי בן חיים <thh123321@gmail.com>
2025-02-22 17:06:52 +0200

    Feat: added some images

--- lib/style/card_style.dart
+++ lib/style/card_style.dart
@@ -4,8 +4,35 @@ import 'package:playing_cards/playing_cards.dart';
 final defaultCardStyle = PlayingCardViewStyle(
   surfaceTintColor: Colors.yellowAccent,
   suitStyles: {
-    Suit.spades: SuitStyle(),
-    Suit.clubs: SuitStyle(),
+    Suit.spades: SuitStyle(
+      cardContentBuilders: {
+        CardValue.two: (context) => Image.asset("assets/monsters/kobold.jpg"),
+        CardValue.three:
+            (context) => Image.asset("assets/monsters/skeleton.jpg"),
+        CardValue.four:
+            (context) => Image.asset("assets/monsters/hell hound.webp"),
+        CardValue.five: (context) => Image.asset("assets/monsters/ghost.webp"),
+        CardValue.six:
+            (context) => Image.asset("assets/monsters/peagasus.webp"),
+        CardValue.seven: (context) => Image.asset("assets/monsters/hag.webp"),
+        CardValue.eight:
+            (context) => Image.asset("assets/monsters/minotaur.webp"),
+      },
+    ),
+    Suit.clubs: SuitStyle(
+      cardContentBuilders: {
+        CardValue.two: (context) => Image.asset("assets/monsters/rat.jpg"),
+        CardValue.three:
+            (context) => Image.asset("assets/monsters/goblin.webp"),
+        CardValue.four: (context) => Image.asset("assets/monsters/zombie.webp"),
+        CardValue.five:
+            (context) => Image.asset("assets/monsters/werewolf.webp"),
+        CardValue.six: (context) => Image.asset("assets/monsters/orc.webp"),
+        CardValue.seven: (context) => Image.asset("assets/monsters/ghoul.jpg"),
+        CardValue.eight:
+            (context) => Image.asset("assets/monsters/yuan ti.jpg"),
+      },
+    ),
     Suit.hearts: SuitStyle(),
     Suit.diamonds: SuitStyle(),
   },
